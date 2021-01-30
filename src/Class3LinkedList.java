0b7804ade75488ab60040b3770cb0f055bd5a79e
Yuchao Zhang <35940382+yc450894425@users.noreply.github.com>
2021-01-29 23:08:50 -0500

    Class 3 LinkedList

--- src/Class3LinkedList.java
+++ src/Class3LinkedList.java
@@ -1,3 +1,49 @@
 public class Class3LinkedList {
+    public ListNode reverse(ListNode head) {
+        // base cases
+        if (head.next == null) {
+            return head;
+        }
+        // recursive rules
+        ListNode newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+//    C:	input => ListNode head
+//    output => ListNode mid
+//    A:	1 => 2 => 3, return 2;
+//          1 => 2 => 3 => 4, return 2
+//    R:
+//      Highlevel: fast and slow pointers;
+    public ListNode middleNode(ListNode head) {
+        // corner cases
+        if (head == null) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // Use fast and slow pointers to check if there are cycles.
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
 }
