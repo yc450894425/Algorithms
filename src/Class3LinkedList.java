48cd07736fb5e2cc7be8edf84ae03e9d34af11ee
Yuchao Zhang <35940382+yc450894425@users.noreply.github.com>
2021-02-02 22:07:51 -0500

    Class4 QueueAndStack Initialization

--- src/Class3LinkedList.java
+++ src/Class3LinkedList.java
@@ -139,5 +139,70 @@ public class Class3LinkedList {
         return merge(mergeSort(head), mergeSort(right));
     }
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // corner cases
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        int value = 0;
+        while (l1 != null || l2 != null || value != 0) {
+            if (l1 != null) {
+                value += l1.value;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                value += l2.value;
+                l2 = l2.next;
+            }
+            cur.next = new ListNode(value % 10);
+            cur = cur.next;
+            value = value / 10;
+        }
+        return dummy.next;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        // corner cases
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode mid = middleNode(head);
+        ListNode right = reverse(mid.next);
+
+        while (right != null) {
+            if (head.value != right.value) {
+                return false;
+            }
+            head = head.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        // corner cases
+        if (head == null) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+
+        while (cur.next != null) {
+            if (cur.next.value == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
 
 }
