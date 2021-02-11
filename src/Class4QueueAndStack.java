d526e4dcb18c6fef054c1a93864fb4cbe7b3ed5c
Yuchao Zhang <35940382+yc450894425@users.noreply.github.com>
2021-02-11 12:39:51 -0500

    Class 5 Binary Tree

--- src/Class4QueueAndStack.java
+++ src/Class4QueueAndStack.java
@@ -194,5 +194,75 @@ public class Class4QueueAndStack {
         }
     }
 
+    public static class DequeByThreeStacks {
+
+        // left is first, right is last;
+        Deque<Integer> left;
+        Deque<Integer> right;
+        Deque<Integer> buffer;
+
+        public DequeByThreeStacks() {
+            left = new ArrayDeque<>();
+            right = new ArrayDeque<>();
+            buffer = new ArrayDeque<>();
+        }
+
+        public void offerFirst(int element) {
+            left.offerFirst(element);
+        }
+
+        public void offerLast(int element) {
+            right.offerFirst(element);
+        }
+
+        public Integer peekFirst() {
+            move(right, left);
+            return left.isEmpty() ? null : left.peekFirst();
+        }
+
+        public Integer peekLast() {
+            move(left, right);
+            return right.isEmpty() ? null : left.peekFirst();
+        }
+
+        public Integer pollFirst() {
+            move(right, left);
+            return left.isEmpty() ? null : left.pollFirst();
+        }
+
+        public Integer pollLast() {
+            move(left, right);
+            return right.isEmpty() ? null : right.pollFirst();
+        }
+
+        public boolean isEmpty() {
+            return left.isEmpty() && right.isEmpty();
+        }
+
+        public int size() {
+            return left.size() + right.size();
+        }
+
+        private void move(Deque<Integer> src, Deque<Integer> dest) {
+            if (!dest.isEmpty()) {
+                return;
+            }
+
+            int halfSize = src.size() / 2;
+
+            for (int i = 0; i < halfSize; i++) {
+                buffer.offerFirst(src.pollFirst());
+            }
+
+            while (!src.isEmpty()) {
+                dest.offerFirst(src.pollFirst());
+            }
+
+            while (!buffer.isEmpty()) {
+                src.offerFirst(buffer.pollFirst());
+            }
+        }
+    }
+
 
 }
