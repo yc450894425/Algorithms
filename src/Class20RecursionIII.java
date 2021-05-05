13cd9765f7c656fbae9b5bd5243c0ea31cd28d39
Yuchao Zhang <35940382+yc450894425@users.noreply.github.com>
2021-05-05 12:00:22 -0400

    Recursion III

--- src/Class20RecursionIII.java
+++ src/Class20RecursionIII.java
@@ -1,18 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Class20RecursionIII {
 
     public int maxPathSumAnyToAny(TreeNode root) {
         int[] max = new int[]{Integer.MIN_VALUE};
-        maxPathSumHelper(root, max);
+        maxPathSumAnyToAnyHelper(root, max);
         return max[0];
     }
-    private int maxPathSumHelper(TreeNode root, int[] max) {
+    private int maxPathSumAnyToAnyHelper(TreeNode root, int[] max) {
         // base case
         if (root == null) {
             return 0;
         }
         // recursive rule
-        int left = maxPathSumHelper(root.left, max);
-        int right = maxPathSumHelper(root.right, max);
+        int left = maxPathSumAnyToAnyHelper(root.left, max);
+        int right = maxPathSumAnyToAnyHelper(root.right, max);
         left = Math.max(left, 0);
         right = Math.max(right, 0);
         max[0] = Math.max(root.key + left + right, max[0]);
@@ -24,12 +27,18 @@ public class Class20RecursionIII {
     * */
     public int maxPathSumLeafToRoot(TreeNode root) {
         // base case
-        // We shouldn't return 0 when root == null, because the max sum might be negative.
+        // If the input node is null, the max sum is 0 of course.
+        // This is unnecessary because we assume root != null and this could be confusing, so I commented the base case.
+        // But you should know the base case is root == null, not that root is a leaf. Actually the latter is a part of recursive rules.
+//        if (root == null) {
+//            return 0;
+//        }
+        // recursive rule
+        // We shouldn't really return 0 when root == null, because the max sum might be negative.
         // if root is a leaf node
         if (root.left == null && root.right == null) {
             return root.key;
         }
-        // recursive rule
         // if root.left == null, we just add right result on root.key
         if (root.left == null) {
             return maxPathSumLeafToRoot(root.right) + root.key;
@@ -39,6 +48,31 @@ public class Class20RecursionIII {
             return maxPathSumLeafToRoot(root.left) + root.key;
         }
         return root.key + Math.max(maxPathSumLeafToRoot(root.left), maxPathSumLeafToRoot(root.right));
+    }
+    /*  Key point: transfer preSum from top to bottom.
+    * */
+    public int maxPathSumLeafToRootII(TreeNode root) {
+        int[] max = new int[]{Integer.MIN_VALUE};
+        maxPathSumLeafToRootIIHelper(root, 0, max);
+        return max[0];
+    }
+    /*  The semantic of preSum is the sum from root to the current node.
+        Transfer sum up to down.
+    * */
+    private void maxPathSumLeafToRootIIHelper(TreeNode root, int preSum, int[] max) {
+        // base case
+        if (root == null) {
+            return;
+        }
+        // recursive rule
+        preSum += root.key;
+        // if it's a leaf
+        if (root.left == null && root.right == null) {
+            max[0] = Math.max(preSum, max[0]);
+            return;
+        }
+        maxPathSumLeafToRootIIHelper(root.left, preSum, max);
+        maxPathSumLeafToRootIIHelper(root.right, preSum, max);
     }
 
     public int maxPathSumLeafToLeaf(TreeNode root) {
@@ -72,7 +106,76 @@ public class Class20RecursionIII {
         return root.key + (root.left == null ? right : left);
     }
 
-//    public boolean pathSumToTarget(TreeNode root, int target) {
-//
-//    }
+    /*  Key point: How to record the all possible sums of visited nodes.
+            We use a set which contains all path prefix sum from root to current node.
+
+            target: 17
+                5 => 11 => 6
+          set   5    16   22
+
+            Update curr prefix sum immediately once we arrived at a new node.
+          if set.contains(curr prefix sum - target), we can return true;
+    * */
+    public boolean pathSumToTarget(TreeNode root, int target) {
+        Set<Integer> set = new HashSet<>();
+        set.add(0);
+        return pathSumToTargetHelper(root, 0, set, target);
+    }
+    private boolean pathSumToTargetHelper(TreeNode root, int preSum, Set<Integer> set, int target) {
+        // base case
+        if (root == null) {
+            return false;
+        }
+        // recursive rule
+        preSum += root.key;
+        if (set.contains(preSum -  target)) {
+            return true;
+        }
+        boolean notExisted = set.add(preSum);
+        boolean left = pathSumToTargetHelper(root.left, preSum, set, target);
+        boolean right = pathSumToTargetHelper(root.right, preSum, set, target);
+        if (left || right) {
+            return true;
+        }
+        if (notExisted) {
+            set.remove(preSum);
+        }
+        return false;
+    }
+
+    public int maxPathSumOnStraightPath(TreeNode root) {
+        int[] max = new int[] {Integer.MIN_VALUE};
+//        maxPathSumOnStraightPathHelperI(root, 0, max);
+        maxPathSumOnStraightPathHelperII(root, max);
+        return max[0];
+    }
+    /*  The semantic of the preSum is the max sum ending at current node.
+        Transfer sum up to down.
+    * */
+    private void maxPathSumOnStraightPathHelperI(TreeNode root, int preSum, int[] max) {
+        // base case
+        if (root == null) {
+            return;
+        }
+        // recursive rule
+        preSum = preSum > 0 ? preSum + root.key : root.key;
+        max[0] = Math.max(preSum, max[0]);
+        maxPathSumOnStraightPathHelperI(root.left, preSum, max);
+        maxPathSumOnStraightPathHelperI(root.right, preSum, max);
+    }
+    /*  The semantic of the helper function is returning the max sum starting from the current node.
+        Transfer sum down to up.
+    * */
+    private int maxPathSumOnStraightPathHelperII(TreeNode root, int[] max) {
+        // base case
+        if (root == null) {
+            return 0;
+        }
+        // recursive rule
+        int left = Math.max(maxPathSumOnStraightPathHelperII(root.left, max), 0);
+        int right = Math.max(maxPathSumOnStraightPathHelperII(root.right, max), 0);
+        int curr = root.key + Math.max(left, right);
+        max[0] = Math.max(max[0], curr);
+        return curr;
+    }
 }
