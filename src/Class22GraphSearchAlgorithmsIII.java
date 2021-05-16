10a724c71bd7dfd74052c244ef8fc65318193221
Yuchao Zhang <35940382+yc450894425@users.noreply.github.com>
2021-05-15 21:44:02 -0400

    Class 22 Graph Search Algorithms III

--- src/Class22GraphSearchAlgorithmsIII.java
+++ src/Class22GraphSearchAlgorithmsIII.java
@@ -1,3 +1,8 @@
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 public class Class22GraphSearchAlgorithmsIII {
     /*
 	Key point:
@@ -46,5 +51,64 @@ if set[i] & set[j] != 0, word i and j have common chars.
         }
         return globMax;
     }
+
+    /*
+	the smallest one is: 3 * 5 * 7L
+key point:
+	How to deduplicate?
+		Set<Long>
+	When to deduplicate?
+		Before we insert a new element into minHeap
+	How to get all neighbors of one node?
+		node * 3, node * 5, node * 7
+
+	Data structure and Initialization:
+Set<Long> visited = new HashSet<>();
+PriorityQueue<Long> minHeap = new PriorityQueue<>(new Comparator<Long>() {
+	@Override
+	public int compare(Long n1, Long n2) {
+	// compare
+}
+});
+visited.add(3 * 5 * 7L);
+minHeap.offer(3 * 5 * 7L);
+
+For each step:
+	poll out the min element curr from minHeap
+	add {curr * 3, curr * 5, curr * 7} which is not visited yet into minHeap, mark them as visited
+Terminate:
+	i == k - 1
+
+Return:
+	minHeap.poll();
+Time Complexity:
+	O(log(2k) * k) => O(klogk)
+Space Complexity:
+	O(2k) for visited
+	O(2k) for minHeap
+	=> O(k) for total
+*/
+    public long kth(int k) {
+        Set<Long> visited = new HashSet<>();
+        PriorityQueue<Long> minHeap = new PriorityQueue<>();
+        visited.add(3 * 5 * 7L);
+        minHeap.offer(3 * 5 * 7L);
+        k--;
+        while (k > 0) {
+            Long curr = minHeap.poll();
+            if (visited.add(curr * 3)) {
+                minHeap.offer(curr * 3);
+            }
+            if (visited.add(curr * 5)) {
+                minHeap.offer(curr * 5);
+            }
+            if (visited.add(curr * 7)) {
+                minHeap.offer(curr * 7);
+            }
+            k--;
+        }
+        return minHeap.poll();
+    }
+
 
 }
